f1242248dc81b0e6cfad8ef823ab98695c0a3128
randern212 <randern212@gmail.com>
2025-11-11 12:25:01 +0200

    added automatic testing to the application

--- NewspaperSellerSimulation/Program.cs
+++ NewspaperSellerSimulation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NewspaperSellerModels;
 using NewspaperSellerTesting;
 
 namespace NewspaperSellerSimulation
@@ -15,9 +16,18 @@ namespace NewspaperSellerSimulation
         [STAThread]
         static void Main()
         {
+            SimulationSystem system=new SimulationSystem();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+            List<string> testCaseNames = new List<string> { Constants.FileNames.TestCase1, Constants.FileNames.TestCase2, Constants.FileNames.TestCase3 }; ;
+            foreach(string testCase in testCaseNames)
+            {
+                string result = TestingManager.Test(system, testCase);
+                MessageBox.Show(result);
+            }
         }
     }
 }
